79a048114d140bd3f45575d882629038d8c61ee6
Vangarikarthik <vangarinani@gmail.com>
2024-03-04 18:46:47 +0530

    adding files to git

---
+++ app.py
@@ -0,0 +1,20 @@
+ from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    expression = request.form['expression']
+    try:
+        result = eval(expression)
+    except Exception as e:
+        result = str(e)
+    return render_template('result.html', expression=expression, result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
