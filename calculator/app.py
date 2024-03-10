c6f53036e2fc41b176aa66bede61f19b71be0769
Vangarikarthik <vangarinani@gmail.com>
2024-03-10 16:18:11 +0530

    changed app

--- calculator/app.py
+++ calculator/app.py
@@ -16,5 +16,5 @@ def calculate():
     return render_template('result.html', expression=expression, result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
